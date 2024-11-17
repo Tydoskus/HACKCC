25ac7b59fdf505f95b33d066fb6d3aed62a18988
Serena <nguyenmngoc445@gmail.com>
2024-11-17 12:16:00 -0800

    change to python

--- main.cpp
+++ main.cpp
@@ -1,65 +1,43 @@
-#include <iostream>
-#include <cmath>
-#include <random>
+R = 3963.1; 
+# in miles
+PI = 3.14159265358979;
 
-using namespace std;
-
-const double R = 3963.1; // in miles
-const double PI = M_PI;
-
-double toRadians(double degree) {
-    return degree * (PI / 180.0); //convert to rad by times with pi/180
-}
-
-double calculate_distance(double lat1, double lon1, double lat2, double lon2)
-{
-    lat1 = toRadians(lat1);
-    lon1 = toRadians(lon1);
-    lat2 = toRadians(lat2);
-    lon2 = toRadians(lon2);
+def calculate_distance( lat1,  lon1,  lat2, lon2):
+    lat1 = math.radians(lat1);
+    lon1 = math.radians(lon1);
+    lat2 = math.radians(lat2);
+    lon2 = math.radians(lon2);
     
-    double dlat = lat2 - lat1;
-    double dlon = lon2 - lon1;
+     dlat = lat2 - lat1;
+     dlon = lon2 - lon1;
     
-    double a = sin(dlat / 2) * sin(dlat / 2) +
-               cos(lat1) * cos(lat2) * 
-               sin(dlon / 2) * sin(dlon / 2);
-    double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+     a = math.sin(dlat / 2) * math.sin(dlat / 2) +
+               math.cos(lat1) * math.cos(lat2) * 
+               math.sin(dlon / 2) * math.sin(dlon / 2);
+     c = 2 * math.atan2(sqrt(a), math.sqrt(1 - a));
     
-    double distance = R * c;
+    distance = R * c;
     
     return distance;
 };
 
-void sendAlert(double newDistance)
-{
-    cout << newDistance;
-};
 
 int main() {
-    cout << "testing" << endl;
+    #cout << "testing" << endl;
+     amb_lat = 0;
+     amb_lon = 0;
 
-    // the random is just for the demo purpose!
-
-    double amb_lat = rand() % 181 -90;
-    double amb_lon = rand() % 361 -180;
-
-    // need to find cars on the route of the ambulance
-    //need latitude and longtitude of several points on the path
-
-    double veh_lat = rand() % 181 -90; // this car should be found to be on the route of the ambulance
-
-    double veh_lon = rand() % 361 -180;// still that car
-
-    double distance = calculate_distance(amb_lat, amb_lon, veh_lat, veh_lon);
+    # num of cars on the route = ???
+    # while ??? > 0
+    
+     veh_lat =0; 
+     veh_lon = 0;
+     distance = calculate_distance(amb_lat, amb_lon, veh_lat, veh_lon);
      
-    cout << "distance: " << distance << endl;
+    print ("distance: " , distance , end="\n")
 
-    if (distance <= 1)
-    {
-        //sendAlert(distance);
-        cout << "Alert!!! Ambulance is on the way. Pull to the right." << endl;
-    }
-    return 0;
+    if distance <= 1:
+        print () "Alert!!! Ambulance is on the way. Pull to the right." )
+    
 }
 
